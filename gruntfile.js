d66443726b88640bcc4fc332289f4aa6ed996b09
Dimas Prameshwara <Zetzumarshen@gmail.com>
2016-09-24 07:20:39 +0700

    using wiredep to autogenerate css and js bower packages

--- gruntfile.js
+++ gruntfile.js
@@ -1,59 +1,19 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        bower_concat: {
-            build: {
-                dest: {
-                    'js': 'assets/js/vendor.js',
-                    'css': 'assets/css/vendor.css'
-                },
-                mainFiles: {
-                    'bootstrap': [
-                        'dist/css/bootstrap.css',
-                        'dist/js/bootstrap.js'
-                    ]
-                },
-                exclude: ['jquery'],
-                bowerOptions: {
-                    relative: false
-                }
-            }
-        },
-        uglify: {
-            build: {
-                files: [{
-                    expand: true,
-                    ext: '.min.js',
-                    src: 'assets/**/*.js'
-                }]
-            }
-        },
-        cssmin: {
-            build: {
-                files: [{
-                    expand: true,
-                    src: 'assets/**/*.css',
-                    ext: '.min.css'
-                }]
-            }
-        },
-        copy: {
-            copy_fonts: {
-                files: [
-                    // include files in every folder named fonts
-                    {expand: true, flatten: true, src: ['**/fonts/*'], dest: 'assets/fonts', filter: 'isFile'},
-                ]
+        wiredep: {
+            task: {
+                src: 'adminlte.html',
+                exclude: [/\/jquery\//,/\/jquery-ui\// ]
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-bower-concat');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-wiredep')
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy')
 
-    grunt.registerTask('default', ['bower_concat', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['wiredep']);
 
-    grunt.registerTask('copy_fonts', ['copy']);
-};
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,13 @@
-TODO: remove `grunt-wiredep grunt-contrib-watch`
-
 .bower.json is pointed by bower-concat mainFiles. Remove or alter it to pick adminLTE skin.
  
+ listed plugin in adminLTE folder (excluding jQuery)
  
+`bower install bootstrap-slider bootstrap-wysihtml5 chartjs ckeditor colorpicker datatables datepicker fastclick flot fullcalendar iCheck jquery.inputmask ionrangeslider jquery-ui jvectormap knob morrisjs pace select2 slimScroll sparkline bootstrap-timepicker bootstrap components-font-awesome ionicons modernize jquery-knob --savegrunt -S`
+
+These bowerpackages are broken; using AdminLTE/plugins/<plugin_name> instead
+`daterangepicker` `moment` `jquery.knob` 
+ 
+These bower packages do not have main in their .bower.json
+ `datepicker` `fullcalendar` `bootstrap`
+
+jQuery and jQuery-UI is using external static file 
